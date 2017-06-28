d62282b3208c707263d62812e0549dd8a99c7f6a
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-06-28 10:36:52 -0700

    Initial Website Push
    Using for live test

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+var currentSlide = 1;
+focusDiv(currentSlide);
+
+function plus(i) {
+  if(i<30){
+    if(i>0 && (currentSlide==5 || currentSlide==15)){
+      focusDiv(currentSlide+=2);
+    } else if(i<0 && (currentSlide==7 || currentSlide==17)) {
+            focusDiv(currentSlide-=2);
+            } else {
+              focusDiv(currentSlide+=i);
+            }
+  } else {
+    focusDiv(currentSlide+=1);
+  }
+}
+
+function mgmtHome() {
+  focusDiv(currentSlide=7);
+}
+
+function fiscalHome() {
+  focusDiv(currentSlide=13);
+}
+
+function exSavings() {
+  focusDiv(currentSlide=16);
+}
+
+function mainDiv() {
+  focusDiv(currentSlide=1);
+}
+
+function focusDiv(i) {
+  var slides = document.getElementsByClassName("slide");
+  if(i > slides.length) {
+    currentSlide=1;
+  }
+  if(i < 1) {
+    currentSlide=slides.length;
+  }
+  var j;
+  for(j=0;j<slides.length;j++){
+    slides[j].style.display = "none";
+  }
+  if(currentSlide==1 || currentSlide==7 || currentSlide==13){
+    document.getElementsByClassName("buttonRow")[0].style.backgroundColor = "blue";
+  } else {
+    document.getElementsByClassName("buttonRow")[0].style.backgroundColor = "#696969";
+  }
+  slides[currentSlide-1].style.display = "block"
+}
+
+$( document ).ready(function() {
+    $("button").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 800);
+});
+});
